4df2569a89cc3faf9ef23f55a176201544699704
Carlos <crlco@uol.com.br>
2022-06-28 14:51:29 -0300

    Criado metodo de Classe @staticmethod e @classmethod

--- orbj/pessoa.py
+++ orbj/pessoa.py
@@ -16,6 +16,7 @@ class Pessoa:
     def metodo_estatico():
         return 42
     # outro método de Classe: classmethod
+    @classmethod
     def nome_e_atributos_de_classe(cls):
         return(f'{cls} - olhos {cls.olhos}')
 
@@ -41,7 +42,7 @@ if __name__ == '__main__':
     print(f'Olhos Carlos: {carlos.olhos}')
     print(f'Olhos Eduardo: {eduardo.olhos}')
     print(id(Pessoa.olhos), id(carlos.olhos), id(eduardo.olhos))
-    print(Pessoa.metodo_estatico(), eduardo.metodo_estatico())
+    print(Pessoa.metodo_estatico(), carlos.metodo_estatico())
     # executando pela Classe
     print(Pessoa.nome_e_atributos_de_classe())
     # executando diretamente através do objeto
